259354cedf7fda7e67ad84fd0ab01d5850131089
Leo Arantes <leobalionis@hotmail.com>
2022-12-04 11:45:39 -0300

    :bug: Correção da transcrição própria

--- client/src/components/meeting/MeetingContainers/TranscriptionContainer/index.jsx
+++ client/src/components/meeting/MeetingContainers/TranscriptionContainer/index.jsx
@@ -9,71 +9,58 @@ export default function TranscriptionContainer({ Size }) {
 	const socketClientRef = useRef();
 
 	useEffect(() => {
-		const client = getSocket();
-		client.on("transcription", (transcription) => {
-			//console.log("transcription received");
-			setText(transcription.sender + ": " + transcription.transcription);
+		socketClientRef.current = getSocket();
+		socketClientRef.current.on("transcription", (transcription) => {
+			console.log("transcription received", transcription);
+			if (
+				userName !== "" &&
+				transcription.sender !== userName &&
+				transcription.sender !== ""
+			) {
+				setText(
+					`${transcription.sender}: ${transcription.transcription}`
+				);
+			}
 		});
-		socketClientRef.current = client;
-	}, []);
+	}, [userName]);
 
 	if (window.electron) {
 		window.electron.EmotionAnalyzer((args) => {
+			if (userName !== "") {
+				let current = new Date();
+				String(current.getMinutes()).padStart(2, "0");
+				let time =
+					String(current.getHours()).padStart(2, "0") +
+					":" +
+					String(current.getMinutes()).padStart(2, "0") +
+					":" +
+					String(current.getSeconds()).padStart(2, "0") +
+					"." +
+					String(current.getMilliseconds()).padStart(3, "0");
 
-			let current = new Date();
-			String(current.getMinutes()).padStart(2, "0");
-			let time =
-				String(current.getHours()).padStart(2, "0") +
-				":" +
-				String(current.getMinutes()).padStart(2, "0") +
-				":" +
-				String(current.getSeconds()).padStart(2, "0") +
-				"." +
-				String(current.getMilliseconds()).padStart(3, "0");
-
-			socketClientRef.current.emit("transcription", {
-				message: {
-					transcrition: args.replace(
-						/SENTIMENTO POSITIVO|SENTIMENTO NEGATIVO|SENTIMENTO NEUTRO/gi,
-						function (x) {
-							switch (x) {
-								case "SENTIMENTO POSITIVO":
-									return "🙂";
-								case "SENTIMENTO NEGATIVO":
-									return "🙁";
-								case "SENTIMENTO NEUTRO":
-								default:
-									return "😐";
+				socketClientRef.current.emit("transcription", {
+					message: {
+						transcription: args.replace(
+							/SENTIMENTO POSITIVO|SENTIMENTO NEGATIVO|SENTIMENTO NEUTRO/gi,
+							function (x) {
+								switch (x) {
+									case "SENTIMENTO POSITIVO":
+										return "🙂";
+									case "SENTIMENTO NEGATIVO":
+										return "🙁";
+									case "SENTIMENTO NEUTRO":
+									default:
+										return "😐";
+								}
 							}
-						}
-					),
-					sender: userName,
-					time: time,
-				},
-				room: getRoomCode(),
-			});
-
-			// setText(
-			// 	args.replace(
-			// 		/SENTIMENTO POSITIVO|SENTIMENTO NEGATIVO|SENTIMENTO NEUTRO/gi,
-			// 		function (x) {
-			// 			switch (x) {
-			// 				case "SENTIMENTO POSITIVO":
-			// 					return "🙂";
-			// 				case "SENTIMENTO NEGATIVO":
-			// 					return "🙁";
-			// 				case "SENTIMENTO NEUTRO":
-			// 				default:
-			// 					return "😐";
-			// 			}
-			// 		}
-			// 	)
-			// );
+						),
+						sender: userName,
+						time: time,
+					},
+					room: getRoomCode(),
+				});
+			}
 		});
 	}
-	return (
-		<Container Size={Size}>
-			{text}
-		</Container>
-	);
+	return <Container Size={Size}>{text}</Container>;
 }
